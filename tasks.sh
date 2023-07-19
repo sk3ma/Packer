1fdaa30139010685d995efe8f0668bfb6fbb41a5
Levon Eakin <52137438+sk3ma@users.noreply.github.com>
2023-07-19 08:44:39 +0200

    Update tasks.sh

--- tasks.sh
+++ tasks.sh
@@ -3,15 +3,15 @@
 # Installing packages.
 echo "[ INFO ] Installing packages..."
 apt-get clean
-apt install vim -y
-apt install screenfetch -y
-apt install git -y
-apt install net-tools -y
-apt install pv -y
-apt install screen -y
-apt install sysstat -y
-apt install unzip -y
-apt install wget -y
-apt install curl -y
+apt install vim  \
+    screenfetch  \
+    git  \
+    net-tools  \
+    pv  \
+    screen  \
+    sysstat  \
+    unzip  \
+    wget  \
+    curl -y
 apt remove unattended-upgrades -y
 apt autoremove -y
