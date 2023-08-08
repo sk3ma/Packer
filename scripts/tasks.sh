4ed683a20501e6dac18b6909c938b7a70e740c4b
Levon Eakin <52137438+sk3ma@users.noreply.github.com>
2023-08-08 09:05:32 +0200

    Re-added files

---
+++ scripts/tasks.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -eux
+
+# Installing packages.
+echo "==> Installing packages..."
+apt-get clean
+apt install -y vim  \
+    screenfetch  \
+    git  \
+    net-tools  \
+    pv  \
+    screen  \
+    sysstat  \
+    unzip  \
+    wget  \
+    curl
+apt remove unattended-upgrades -y
+apt autoremove -y
