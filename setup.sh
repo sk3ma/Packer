ab3dde7150a5fbe919e7c0690c5c71fe27286013
Levon Eakin <52137438+sk3ma@users.noreply.github.com>
2023-08-08 09:02:23 +0200

    Delete setup.sh

--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -eux
-
-sudo systemctl start ssh
-
-# Alter user account.
-echo "ubuntu        ALL=(ALL)       NOPASSWD: ALL" | sudo tee -a /etc/sudoers.d/ubuntu
-sudo sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers.d/ubuntu
